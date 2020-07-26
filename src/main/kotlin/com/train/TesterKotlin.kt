db6f234a3da1f34fde80e73f09f602805a11e365
ddssads <tw0937608360@gmail.com>
2020-07-26 22:35:25 +0800

    Java and Kotlin added the features with while for continued purchase.

--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -4,12 +4,17 @@ import java.util.*
 
 fun main() {
     var scanner = Scanner(System.`in`)
-    print("Please enter number of tickets: ")
-    var totaltickets = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    var roundtriptickets = scanner.nextInt()
-    var ticketprice = Ticket(totaltickets,roundtriptickets)
-    ticketprice.print()
+    var totaltickets = 0
+    while(totaltickets != -1) {
+        print("Please enter number of tickets: ")
+        totaltickets = scanner.nextInt()
+        print("How many round-trip tickets: ")
+        var roundtriptickets = scanner.nextInt()
+        var ticketprice = Ticket(totaltickets, roundtriptickets)
+        ticketprice.print()
+        print("Enter the -1 if you don't buy anymore")
+        totaltickets = scanner.nextInt()
+    }
 }
 
 class Ticket(var totaltickets : Int, var roundtriptickets : Int){
